52b2b0ed2c85bcd39dde2e2620d57ab5c09fe0c8
Gang Chen <ssword@gmail.com>
2018-05-18 20:04:10 -0500

    working on adding SHIFT key support

--- src/Hello.js
+++ src/Hello.js
@@ -1,6 +1,8 @@
 import React from "react";
 import PropTypes from "prop-types";
-import { withStyles } from "@material-ui/core/styles";
+import {
+  withStyles
+} from "@material-ui/core/styles";
 import Input from "@material-ui/core/Input";
 import InputLabel from "@material-ui/core/InputLabel";
 import MenuItem from "@material-ui/core/MenuItem";
@@ -41,7 +43,7 @@ const MenuProps = {
 };
 
 const names = [
-  "All", 
+  "All",
   "Oliver Hansen",
   "Van Henry",
   "April Tucker",
@@ -55,84 +57,141 @@ const names = [
 ];
 
 class MultipleSelect extends React.Component {
-  state = {
-    name: []
-  };
+    state = {
+      name: [],
+      lastChecked: null
+    };
 
-  handleChange = event => {
+    handleChange = event => {
 
-    var clickedItem = event.currentTarget.attributes['data-value'].nodeValue
-    var selectedItems = event.target.value
-    if (clickedItem === 'All'){
-      if (selectedItems.length < names.length-1){
-      this.setState({name: names});
+      var clickedItem = event.currentTarget.attributes['data-value'].nodeValue
+      var selectedItems = event.target.value
+      console.log(selectedItems)
+      if (clickedItem === 'All') {
+        if (selectedItems.length < names.length - 1) {
+          this.setState({
+            name: names,
+            lastChecked: null
+          });
+        } else {
+          this.setState({
+            name: [],
+            lastChecked: null
+          });
+        }
       } else {
-        this.setState({name: []});
+        var index = selectedItems.indexOf('All');
+        if (index > -1) {
+          selectedItems.splice(index, 1)
+        }
+
+        if(event.shiftKey & this.state.lastChecked){
+            var startIndex = names.indexOf(this.state.lastChecked);
+            var endIndex = names.indexOf(clickedItem);
+            selectedItems = selectedItems.concat(names.slice(startIndex, endIndex))
+        }
+        this.setState({
+          name: selectedItems,
+          lastChecked: clickedItem
+        });
       }
-    }
-    else {
-      var index = selectedItems.indexOf('All');
-      if(index > -1){
-        selectedItems.splice(index,1)
+    };
+
+    handleDelete = data => () => {
+      const chipData = [...this.state.name];
+      if (data === 'All') {
+        this.setState({name: []})
+      } else {
+        const chipToDelete = chipData.indexOf(data);
+        chipData.splice(chipToDelete, 1);
+        this.setState({
+          name: chipData
+        });
       }
-      this.setState({ name: selectedItems });
-    }
-  };
+    };
 
-  handleDelete = data => () => {
-    // if (data.label === 'React') {
-    //   alert('Why would you want to delete React?! :)'); // eslint-disable-line no-alert
-    //   return;
-    // }
+    render() {
+      const {
+        classes,
+        theme
+      } = this.props;
 
-    const chipData = [...this.state.name];
-    const chipToDelete = chipData.indexOf(data);
-    chipData.splice(chipToDelete, 1);
-    this.setState({ name: chipData });
-  };
-
-  render() {
-    const { classes, theme } = this.props;
-
-    return <div className={classes.root}>
-      <FormControl className={classes.formControl}>
-        <InputLabel htmlFor="select-multiple-checkbox">Facility</InputLabel>
-        <Select multiple value={this.state.name} onChange={this.handleChange} input={<Input id="select-multiple-checkbox" />} renderValue={selected => <div
-          className={classes.chips}
-        >
-          {selected.map(value => (
-            <Chip
-              key={value}
-              label={value}
-              className={classes.chip}
-              onDelete={this.handleDelete(value)}
+      return <div className = {
+          classes.root
+        }>
+        <FormControl className = {
+          classes.formControl
+        } >
+        <InputLabel htmlFor = "select-multiple-checkbox" > 
+        Facility 
+        </InputLabel> 
+        <Select multiple value = {
+          this.state.name
+        }
+      onChange = {
+        this.handleChange
+      }
+      input = { < Input id = "select-multiple-checkbox" />
+      }
+      renderValue = {
+        selected => <div
+        className = {
+          classes.chips
+        }>
+        {
+          selected.map(value => ( 
+            <Chip key = {
+              value
+            }
+            label = {
+              value
+            }
+            className = {
+              classes.chip
+            }
+            onDelete = {
+              this.handleDelete(value)
+            }
             />
-          ))}
-        </div>} MenuProps={MenuProps}>
-          {names.map(name => (
-            <MenuItem
-              key={name}
-              value={name}
-              style={{
-                fontWeight:
-                  this.state.name.indexOf(name) === -1
-                    ? theme.typography.fontWeightRegular
-                    : theme.typography.fontWeightMedium
-              }}
-            >
-              <Checkbox checked={this.state.name.indexOf(name) > -1} />
-              <ListItemText primary={name} />
+          ))
+        } </div>} 
+        MenuProps={MenuProps}> {
+          names.map(name => ( 
+            <MenuItem key = {
+              name
+            }
+            value = {
+              name
+            }
+            style = {
+              {
+                fontWeight: this.state.name.indexOf(name) === -1 ?
+                  theme.typography.fontWeightRegular :
+                  theme.typography.fontWeightMedium
+              }
+            }>
+            <Checkbox checked = {
+              this.state.name.indexOf(name) > -1
+            }
+            /> 
+            <ListItemText primary = {
+              name
+            }
+            /> 
             </MenuItem>
-          ))}
-        </Select>
-      </FormControl>
-    </div>;
-  }
-}
+          ))
+        } 
+        </Select> 
+        </FormControl> 
+        </div>;
+      }
+    }
 
-MultipleSelect.propTypes = {
-  classes: PropTypes.object.isRequired,
-  theme: PropTypes.object.isRequired
-};
+    MultipleSelect.propTypes = {
+      classes: PropTypes.object.isRequired,
+      theme: PropTypes.object.isRequired
+    };
 
-export default withStyles(styles, { withTheme: true })(MultipleSelect);
+    export default withStyles(styles, {
+      withTheme: true
+    })(MultipleSelect);
